04422508d69bfc177c1d05a44cc1a8937ba4a423
JeffNJellyBean <35013297+z331it3cod3r@users.noreply.github.com>
2019-02-13 12:09:09 -0500

    use warning modal

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ const app = new Vue({
     el: '#app',
     data: {
         multiplier: 1,
-        bruhs: 0
+        bruhs: 0,
+        warning: ''
     },
     methods: {
         bruh: function() {
             this.bruhs += this.multiplier;
         },
         warn: function(message) {
-            alert(message);
+            this.warning = message;
+            $('#warning_modal').modal();
         }
     }
 });
