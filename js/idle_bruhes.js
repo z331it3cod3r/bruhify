947ec7932861f7a52c3087f95cc52445bf7f2c94
JeffNJellyBean <35013297+z331it3cod3r@users.noreply.github.com>
2019-02-22 13:38:58 -0500

    Update idle_bruhes.js

--- js/idle_bruhes.js
+++ js/idle_bruhes.js
@@ -2,3 +2,8 @@ const idle_bruh_shop = null;
 onmessage = function(e) {
     idle_bruh_shop = e.data;
 }
+for(obj in idle_bruh_shop) {
+    setInterval(function() {
+        postMessage();
+    }, 1/obj.bps);
+}
