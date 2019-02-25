7e430738eea904edc320bd9424c1dcba68adacd3
JeffNJellyBean <35013297+z331it3cod3r@users.noreply.github.com>
2019-02-25 11:09:09 -0500

    Update idle_bruhes.js

--- js/idle_bruhes.js
+++ js/idle_bruhes.js
@@ -8,13 +8,15 @@ onmessage = function(e) {
         clearInterval(timeout_id);
         timeout_id = timeouts.pop();
     }
-    for(let obj in idle_bruh_shop) {
-        if(obj.owned > 0) {
-            let interval = 1000/(obj.bps * obj.owned).toFixed(3);
-            postMessage(interval.toString());
-            timeouts.push(setInterval(function() {
-                postMessage(true);
-            }, interval));    
+    for(let key, obj in idle_bruh_shop) {
+        if(idle_bruh_shop.hasOwnProperty(key)) {
+            if(obj.owned > 0) {
+                let interval = 1000/(obj.bps * obj.owned).toFixed(3);
+                postMessage(interval.toString());
+                timeouts.push(setInterval(function() {
+                    postMessage(true);
+                }, interval));    
+            }
         }
     }
 };
